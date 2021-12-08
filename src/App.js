08513747ed18b33ae4c2033c4e414951080bc480
Luan Vitor <luan_vittor@hotmail.com>
2021-12-08 19:31:34 -0300

    Header done

--- src/App.js
+++ src/App.js
@@ -3,27 +3,45 @@ import Switch from 'react-bootstrap/esm/Switch';
 import { Route } from 'react-router';
 import Login from './pages/Login';
 import Comidas from './pages/Comidas';
+import Perfil from './pages/Perfil';
+import Bebidas from './pages/Bebidas';
+import ExplorarComidasIngredientes from './pages/ExplorarComidasIngredientes';
+import ExplorarComidas from './pages/ExplorarComidas';
+import Explorar from './pages/Explorar';
+import ExplorarBebidas from './pages/ExplorarBebidas';
+import ReceitasFeitas from './pages/ReceitasFeitas';
+import ReceitasFavoritas from './pages/ReceitasFavoritas';
+import ExplorarComidasArea from './pages/ExplorarComidasArea';
+import ExplorarBebidasIngredientes from './pages/ExplorarBebidasIngredientes';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Login } />
-        <Route path="/comidas" component={ Comidas } />
-        <Route path="/bebidas" />
-        <Route path="/perfil" />
-        <Route path="/comidas/{id-da-receita}" />
-        <Route path="/bebidas/{id-da-receita}" />
-        <Route path="/comidas/{id-da-receita}/in-progress" />
-        <Route path="/bebidas/{id-da-receita}/in-progress" />
-        <Route path="/explorar" />
-        <Route path="/explorar/comidas" />
-        <Route path="/explorar/bebidas" />
-        <Route path="/explorar/comidas/ingredientes" />
-        <Route path="/explorar/bebidas/ingredientes" />
-        <Route path="/explorar/comidas/area" />
-        <Route path="/receitas-feitas" />
-        <Route path="/receitas-favoritas" />
+        <Route exact path="/comidas" component={ Comidas } />
+        <Route exact path="/bebidas" component={ Bebidas } />
+        <Route exact path="/perfil" component={ Perfil } />
+        <Route exact path="/comidas/{id-da-receita}" />
+        <Route exact path="/bebidas/{id-da-receita}" />
+        <Route exact path="/comidas/{id-da-receita}/in-progress" />
+        <Route exact path="/bebidas/{id-da-receita}/in-progress" />
+        <Route exact path="/explorar" component={ Explorar } />
+        <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+        <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+        <Route
+          exact
+          path="/explorar/comidas/ingredientes"
+          component={ ExplorarComidasIngredientes }
+        />
+        <Route
+          exact
+          path="/explorar/bebidas/ingredientes"
+          component={ ExplorarBebidasIngredientes }
+        />
+        <Route exact path="/explorar/comidas/area" component={ ExplorarComidasArea } />
+        <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+        <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
       </Switch>
     </div>
   );
--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Comidas extends Component {
   render() {
     return (
       <div>
+        <Header />
         Paginas de comidas
       </div>
     );
