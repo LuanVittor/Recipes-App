7089ebcaad41b0cd4f51f5156c816e45540d3b71
Luan Vitor <luan_vittor@hotmail.com>
2021-12-08 16:00:33 -0300

    login page done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Switch from 'react-bootstrap/esm/Switch';
+import { Route } from 'react-router';
+import Login from './pages/Login';
+import Comidas from './pages/Comidas';
 
-function App() {
+export default function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ Comidas } />
+        <Route path="/bebidas" />
+        <Route path="/perfil" />
+        <Route path="/comidas/{id-da-receita}" />
+        <Route path="/bebidas/{id-da-receita}" />
+        <Route path="/comidas/{id-da-receita}/in-progress" />
+        <Route path="/bebidas/{id-da-receita}/in-progress" />
+        <Route path="/explorar" />
+        <Route path="/explorar/comidas" />
+        <Route path="/explorar/bebidas" />
+        <Route path="/explorar/comidas/ingredientes" />
+        <Route path="/explorar/bebidas/ingredientes" />
+        <Route path="/explorar/comidas/area" />
+        <Route path="/receitas-feitas" />
+        <Route path="/receitas-favoritas" />
+      </Switch>
     </div>
   );
 }
-
-export default App;
