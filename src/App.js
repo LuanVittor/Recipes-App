b8dfbd26ddc60993615b6c077219b5811a33aa49
Luan Vitor <luan_vittor@hotmail.com>
2021-12-09 18:04:19 -0300

    working on

--- src/App.js
+++ src/App.js
@@ -13,36 +13,40 @@ import ReceitasFeitas from './pages/ReceitasFeitas';
 import ReceitasFavoritas from './pages/ReceitasFavoritas';
 import ExplorarComidasArea from './pages/ExplorarComidasArea';
 import ExplorarBebidasIngredientes from './pages/ExplorarBebidasIngredientes';
+import ApiProvider from './Provider/ApiProvider';
 
 export default function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/comidas" component={ Comidas } />
-        <Route exact path="/bebidas" component={ Bebidas } />
-        <Route exact path="/perfil" component={ Perfil } />
-        <Route exact path="/comidas/{id-da-receita}" />
-        <Route exact path="/bebidas/{id-da-receita}" />
-        <Route exact path="/comidas/{id-da-receita}/in-progress" />
-        <Route exact path="/bebidas/{id-da-receita}/in-progress" />
-        <Route exact path="/explorar" component={ Explorar } />
-        <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
-        <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
-        <Route
-          exact
-          path="/explorar/comidas/ingredientes"
-          component={ ExplorarComidasIngredientes }
-        />
-        <Route
-          exact
-          path="/explorar/bebidas/ingredientes"
-          component={ ExplorarBebidasIngredientes }
-        />
-        <Route exact path="/explorar/comidas/area" component={ ExplorarComidasArea } />
-        <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
-        <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
-      </Switch>
+      <ApiProvider>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/comidas" component={ Comidas } />
+          <Route exact path="/bebidas" component={ Bebidas } />
+          <Route exact path="/perfil" component={ Perfil } />
+          <Route exact path="/comidas/:id" />
+          <Route exact path="/bebidas/:id" />
+          <Route exact path="/comidas/:id/in-progress" />
+          <Route exact path="/bebidas/:id/in-progress" />
+          <Route exact path="/explorar" component={ Explorar } />
+          <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+          <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+          <Route
+            exact
+            path="/explorar/comidas/ingredientes"
+            component={ ExplorarComidasIngredientes }
+          />
+          <Route
+            exact
+            path="/explorar/bebidas/ingredientes"
+            component={ ExplorarBebidasIngredientes }
+          />
+          <Route exact path="/explorar/comidas/area" component={ ExplorarComidasArea } />
+          <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+          <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
+        </Switch>
+      </ApiProvider>
+
     </div>
   );
 }
