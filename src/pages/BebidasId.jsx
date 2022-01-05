a0de759b3429a58ad3d6c31793e38e45f613036d
Luan Vitor <luan_vittor@hotmail.com>
2022-01-05 19:23:14 -0300

    erro arrumado

--- src/pages/BebidasId.jsx
+++ src/pages/BebidasId.jsx
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import Loading from '../components/Loading';
 import RecommendationDrink from '../components/RecommendationDrink';
 import '../css/IniciarReceita.css';
 
 export default function BebidasId(id) {
   const history = useHistory();
   const [responseDrinks, setResponseDrinks] = useState([]);
-
-  const returnById = () => (
-    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id.match.params.id}`)
-      .then((resp) => resp.json())
-      .then((r) => setResponseDrinks(r))
-  );
+  const [loading, setLoading] = useState(true);
+  const MIL = 1000;
 
   useEffect(() => {
-    returnById();
+    (async () => {
+      const resp = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id.match.params.id}`);
+      const r = await resp.json();
+      setResponseDrinks(r);
+      setTimeout(() => {
+        setLoading(false);
+      }, MIL);
+    })();
   }, []);
 
   const { drinks } = responseDrinks;
   const FIFTEEN = 15;
 
+  if (loading) return <Loading />;
+
   return (
     <div>
       {console.log(responseDrinks.length === 0)}
-      {(responseDrinks.length === 0) ? null : (
+      {(responseDrinks.length !== 0 && responseDrinks) && (
         <div>
           {console.log(drinks[0])}
           <img src={ drinks[0].strDrinkThumb } alt="Drink" data-testid="recipe-photo" />
           <h1 data-testid="recipe-title">{drinks[0].strDrink}</h1>
-          <p data-testid="recipe-category">{drinks[0].strCategory}</p>
+          <p data-testid="recipe-category">{drinks[0].strAlcoholic}</p>
           <button type="button" data-testid="share-btn">Compartilhar</button>
           <button type="button" data-testid="favorite-btn">Favoritar</button>
           <div>
@@ -49,14 +55,6 @@ export default function BebidasId(id) {
               }))}
           </div>
           <p data-testid="instructions">{drinks[0].strInstructions}</p>
-          <iframe
-            src={ drinks[0].strYoutube }
-            frameBorder="0"
-            data-testid="video"
-            allow="autoplay; encrypted-media"
-            allowFullScreen
-            title="video"
-          />
           <div>
             <h3>Recomendadas</h3>
             <RecommendationDrink />
